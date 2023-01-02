d1c7fa84ba058854d1886d00d86dbdddf2bd5d90
Jed <doom.slaver2@gmail.com>
2023-01-02 17:09:56 +0800

    insert prompt

---
+++ app/javascript/controllers/notice_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  show() {
+    alert("In the next page, copy paste the JSON code to your application.");
+  }
+}
