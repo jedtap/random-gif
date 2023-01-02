b1526469cde8866880abd008fce2474fa24a8412
Jed <doom.slaver2@gmail.com>
2023-01-02 09:47:32 +0800

    skeletal layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :searches
   get 'pages/home'
+  get 'pages/download'
   root 'pages#home'
 end
