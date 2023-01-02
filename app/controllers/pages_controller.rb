59101aa157c71a717a7ef36996d7c92b400a44dd
Jed <doom.slaver2@gmail.com>
2023-01-02 15:28:51 +0800

    search feature is dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @recent = Search.all.last(5).reverse
 
-    response = RestClient.get('https://api.giphy.com/v1/gifs/translate?api_key=' + ENV["KEY"] + '&s=' + 'eagle')
+    response = RestClient.get('https://api.giphy.com/v1/gifs/translate?api_key=' + ENV["KEY"] + '&s=' + Search.last.query)
     json = JSON.parse(response)
     @url = json['data']['images']['downsized_medium']['url']
   end
