bf4a2e36107acee4f787a26875d35f1b5eb791bf
Jed <doom.slaver2@gmail.com>
2023-01-02 13:43:17 +0800

    giphy api connected

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 
 gem 'cssbundling-rails', '~> 1.1', '>= 1.1.2'
+gem 'dotenv-rails'
+gem 'rest-client', '~> 2.1'
+gem 'json', '~> 2.6', '>= 2.6.3'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
     @recent = Search.all.last(5).reverse
+
+    response = RestClient.get('https://api.giphy.com/v1/gifs/translate?api_key=' + ENV["KEY"] + '&s=' + 'eagle')
+    json = JSON.parse(response)
+    @url = json['data']['images']['downsized_medium']['url']
   end
 
   def download
